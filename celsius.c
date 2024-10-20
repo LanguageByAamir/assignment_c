c4a0ccf5b4b0a35b79858b360696d0f61be43072
LanguageByAamir <zardariaamirali200@gmail.com>
2024-10-20 15:11:43 +0500

    Add files via upload

---
+++ celsius.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main() {
+    float celsius, fahrenheit;
+
+    printf("Enter temperature in Celsius: ");
+    scanf("%f", &celsius);
+
+    fahrenheit = (celsius * 9/5) + 32;
+
+    printf("Temperature in Fahrenheit: %.2f\n", fahrenheit);
+
+
+getch();
+
+
+
+}
