57cc347a1671338900ed33e36a4f730d95f23ccc
LanguageByAamir <zardariaamirali200@gmail.com>
2024-10-20 15:08:09 +0500

    Add files via upload

---
+++ square.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+	int number;
+printf("enter the number : ");
+scanf("%d",&number);
+printf("the square of entered number is %d ",number*number);
+getch();
+}
+
